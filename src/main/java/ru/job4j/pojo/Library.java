31bccda7bde0284566e5345c2abb27b0872b7851
Claptrap-maker <lelik575435@gmail.com>
2023-07-10 19:51:50 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book ozhegov = new Book("Dictionary", 874);
+        Book king = new Book("The Dead Zone", 540);
+        Book austen = new Book("Pride and Prejudice", 416);
+        Book martin = new Book("Clean Code", 464);
+        Book[] books = new Book[] {
+            ozhegov, king, austen, martin
+        };
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getTitle() + " " + books[i].getPages());
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getTitle() + " " + books[i].getPages());
+        }
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getTitle().equals("Clean Code")) {
+                System.out.println(books[i].getTitle() + " " + books[i].getPages());
+            }
+        }
+    }
+}

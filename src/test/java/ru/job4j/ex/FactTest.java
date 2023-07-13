a745d8e7eb983633699d79b56c72eaab8f453163
Claptrap-maker <lelik575435@gmail.com>
2023-07-13 16:48:11 +0300

    0.3. Тестирование исключений с junit [#219366 #429607]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -16,4 +16,11 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
     }
+
+    @Test
+    public void whenValid() {
+        int result = new Fact().calc(3);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
 }

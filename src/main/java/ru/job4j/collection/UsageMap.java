ca46fba326cb467212989ebbf814f89809a9771f
Claptrap-maker <lelik575435@gmail.com>
2023-08-17 20:46:26 +0300

    0. Map, HashMap [#212652 #437336]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("lelik575435@gmail.com", "Иванова Юлия Николаевна");
+        map.put("fabulousNick337@yandex.ru", "Иванова Юлия Николаевна");
+        map.put("lelik575435@gmail.com", "Петров Николай Иванович");
+        map.put("petrov@mail.com", "Иванова Юлия Николаевна");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " | " + value);

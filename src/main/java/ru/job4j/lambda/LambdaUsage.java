47bbd86a3b581491bf5a2ed8810f8c8417eb37e7
Claptrap-maker <lelik575435@gmail.com>
2023-08-20 18:54:09 +0300

    2.1. Лямбда блок [#249208 #438128]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -8,7 +8,7 @@ public class LambdaUsage {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
-            System.out.println("comparing - " + right.length() + " : " + left.length());
+            System.out.println("comparing - " + left.length() + " : " + right.length());
             return Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
